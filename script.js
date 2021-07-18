b9b09c7e997012ebbf09fee51162cc8b20bd435f
icang <76472800+webhub20@users.noreply.github.com>
2021-07-18 16:11:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-window.onload = function() {
-    printTime();
-}
-
-function printTime() {
-    let d = new Date();
-    let hour = d.getHours();
-    let min = d.getMinutes();
-    let sec = d.getSeconds();
-    let day = d.getDay();
-    let date = d.getDate();
-    let month = d.getMonth();
-    let year = d.getFullYear();
-
-    const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
-    const months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-
-    if (hour < 10) {
-        hour = "0" + hour;
+window.addEventListener('load', function() {
+    
+    function printTime() {
+        let d = new Date();
+        let hour = d.getHours();
+        let min = d.getMinutes();
+        let sec = d.getSeconds();
+        let day = d.getDay();
+        let date = d.getDate();
+        let month = d.getMonth();
+        let year = d.getFullYear();
+    
+        const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+        const months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+    
+        if (hour < 10) { hour = "0" + hour; }
+        if (min < 10) { min = "0" + min; }
+        if (sec < 10) { sec = "0" + sec; }
+    
+        document.getElementById('time').innerHTML = hour + ":" + min + ":" + sec;
+        document.getElementById('date').innerHTML = days[day] + ", " + date + " " + months[month] + " " + year;
+    
     }
-    if (min < 10 ) {
-        min = "0" + min;
-    }
-    if (sec < 10 ) {
-        sec = "0" + sec;
-    }
+    setInterval(printTime, 1000);
 
-    document.getElementById('time').innerHTML = hour + ":" + min + ":" + sec;
-    document.getElementById('date').innerHTML = days[day] + ", " + date + " " + months[month] + " " + year;
+});
 
-}
-setInterval(printTime, 1000);
